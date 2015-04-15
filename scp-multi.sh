2585d736a5a742ba34fa967ba3e080701d95c709
Paul Montero <lpaulmp@gmail.com>
2015-04-14 22:40:52 -0500

    Scp a local file to multi servers

--- scp-multi.sh
+++ scp-multi.sh
@@ -6,15 +6,16 @@ then
   echo "$FILE exist"
 
   for i in $@
-    do
-      ser=`cat ~/.ssh/config |cut -d " " -f2 |grep $i`
-      if [ "$i" == "$ser" ] 
-        then
-          echo "$FILE is coping to $ser"
-          scp -r $FILE  $ser:/tmp &2>1
-          echo "Copied"
-        fi
-    done
-  else 
-    echo "You need a file copy" 
+  do
+    ser=`cat ~/.ssh/config |cut -d " " -f2 |grep $i`
+    if [ "$i" == "$ser" ] 
+      then
+        echo "$FILE is coping to $ser"
+        scp -r $FILE  $ser:/tmp &2>1
+        echo "Copied"
+    fi
+  done
+
+else 
+  echo "You need a file copy" 
 fi
